e47279460bd535c650de4a140e9a7e83e1a441b8
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-06-09 11:47:42 +0530

    change

--- project3monkeys/settings.py
+++ project3monkeys/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'project3monkeys.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', default='localhost'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
-    }
+        'NAME': 'database3monkeys',
+        'USER':'myuser',
+        'PASSWORD':'3monkeys',
+        'HOST':'localhost',
+        'PORT':'5432',
+}
 }
 
 
