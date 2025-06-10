a6ae542e6ee6dce61bfc6058a7ef818316694e94
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-06-10 16:45:44 +0530

    ok

--- project3monkeys/settings.py
+++ project3monkeys/settings.py
@@ -13,11 +13,10 @@ from pathlib import Path
 from decouple import config
 import dj_database_url
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -80,7 +79,7 @@ WSGI_APPLICATION = 'project3monkeys.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default':dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
