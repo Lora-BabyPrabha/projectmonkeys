9de739a318fa10848f12619865a76fe79bff7e71
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-06-11 10:59:50 +0530

    ok

--- project3monkeys/settings.py
+++ project3monkeys/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'project3monkeys.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default':dj_database_url.config(default=config('DATABASE_URL'))
+    'default':dj_database_url.config (default=config('DATABASE_URL'))
 }
 
 
